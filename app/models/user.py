db78b17b564b058244b6f5f03e6d6476e8becbb4
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-09-04 00:23:20 +0530

    update schemaas of auth

--- app/models/user.py
+++ app/models/user.py
@@ -1,30 +1,30 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
-
-from app.core.database import Base
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    id: Mapped[int] = mapped_column(
-        primary_key=True,
-        index=True
+class UserCreate(BaseModel):
+    name: str = Field(
+        min_length=2,
+        max_length=100
     )
 
-    name: Mapped[str] = mapped_column(
-        String(100),
-        nullable=False
+    email: EmailStr
+
+    password: str = Field(
+        min_length=6,
+        max_length=100
     )
 
-    email: Mapped[str] = mapped_column(
-        String(255),
-        unique=True,
-        index=True,
-        nullable=False
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+    model_config = ConfigDict(
+        from_attributes=True
     )
 
-    hashed_password: Mapped[str] = mapped_column(
-        String(255),
-        nullable=False
-    )
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
